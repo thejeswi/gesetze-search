9358a558252cf372a67e29c1c41c396109aa47e1
tp47 <tp47@gmx.com>
2018-02-21 02:10:54 +0100

    Reimplemented with Whoosh instead of Xapian

---
+++ search.py
@@ -0,0 +1,44 @@
+"""
+Use Whoosh index files to find law section
+"""
+#!/usr/bin/env python
+
+from whoosh.qparser import QueryParser
+from whoosh.lang.porter import stem
+from whoosh.lang.morph_en import variations
+from whoosh.analysis import StemmingAnalyzer
+import whoosh.index as index
+
+INDEX_DIR = 'db/en'
+
+def search(query_input):
+    ix = index.open_dir(INDEX_DIR)
+    stem_ana = StemmingAnalyzer()
+    query_input_stem = stem_ana(query_input)
+
+    # #How to use the expanded query?
+    # expanded_terms = []
+    # for token in query_input_stem:
+    #     expanded_terms.append(variations(token.text))
+    # print(expanded_terms)
+    response = {}
+    with ix.searcher() as searcher:
+        query = QueryParser("content", ix.schema).parse(query_input)
+        response['query'] = str(query)
+        results = searcher.search(query)
+        results_list = []
+        if results:
+            for r in results:
+                content, law_title, para_n = r.values()
+                if para_n == "":
+                    para_n = None
+                url = (law_title, para_n)
+                score = "{0:.2f}".format(r.score)
+                results_list.append((content, url, score))
+        response['results'] = results_list
+    return response
+if __name__ == '__main__':
+    from pprint import pprint
+    output  = search(input())
+    # ~ pprint(output['results'])
+    pprint(output)
---
+++ wsgi.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from search import search
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/search")
+def index():
+    return render_template("index.html")
+
+@app.route("/query", methods=['POST'])
+def query():
+    query = request.form['query']
+    # ~ langauge = request.form['langauge']
+    langauge = 'en'
+    response = search(query)
+    #return results
+    return render_template("search.html", response = response, query=query, language = langauge)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
